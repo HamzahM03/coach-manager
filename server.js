ab8eab4c3c525c73fd96a78ccff662a1fe052d03
Hamzah Marie <mariehamzah1@gmail.com>
2024-11-27 07:32:06 -0500

    Add login and registration forms, with no logic yet and simple styling.

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
+import login_router from "./routes/login.js";
+import register_router from "./routes/register.js";
+
 
 dotenv.config();
-console.log(process.env) // remove this after you've confirmed it is working
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get("/", (req,res)=>{
+  res.render("index");
+})
+
+app.get('/terms', (req, res) => {
+  res.render('./admin/terms');
+});
+
+app.get('/privacy', (req, res) => {
+  res.render('./admin/privacy');
+});
+
+app.get('/forgot', (req, res) => {
+  res.render('./admin/forgot-credentials'); 
+});
+
+
+app.use("/login", login_router);
+app.use("/register", register_router);
 
 
 app.listen(PORT,()=>{
